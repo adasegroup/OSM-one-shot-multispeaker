4bf617cbe8cdc39ede7cf6a6c8e1633f148543f9
Nikolay <nik.kozyrskiy@gmail.com>
2021-05-11 07:15:14 +0300

    development is in progress

---
+++ src/tts_modules/common/multispeaker.py
@@ -0,0 +1,54 @@
+import torch
+from src.tts_modules.encoder.encoder_manager import EncoderManager
+from src.tts_modules.synthesizer.synthesizer_manager import SynthesizerManager
+from src.tts_modules.vocoder.vocoder_manager import VocoderManager
+
+
+
+class MultispeakerManager:
+    def __init__(self, configs,
+                 encoder=None, encoder_checkpoint_path=None,
+                 encoder_test_dataloader=None, encoder_train_dataloader=None,
+                 synthesizer=None, synthesizer_checkpoint_path=None,
+                 synthesizer_test_dataloader=None, synthesizer_train_dataloader=None,
+                 vocoder=None, vocoder_checkpoint_path=None,
+                 vocoder_test_dataloader=None, vocoder_train_dataloader=None):
+        self.configs = configs
+        self.encoder_manager = EncoderManager(configs,
+                                              model=encoder,
+                                              checkpoint_path=encoder_checkpoint_path,
+                                              test_dataloader=encoder_test_dataloader,
+                                              train_dataloader=encoder_train_dataloader)
+        self.synthesizer_manager = SynthesizerManager(configs,
+                                                      model=synthesizer,
+                                                      checkpoint_path=synthesizer_checkpoint_path,
+                                                      test_dataloader=synthesizer_test_dataloader,
+                                                      train_dataloader=synthesizer_train_dataloader)
+        self.vocoder_manager = VocoderManager(configs,
+                                              model=vocoder,
+                                              checkpoint_path=vocoder_checkpoint_path,
+                                              test_dataloader=vocoder_test_dataloader,
+                                              train_dataloader=vocoder_train_dataloader)
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda")
+        else:
+            self.device = torch.device("cpu")
+
+    def inference(self):
+        pass
+
+    def process_speaker(self, speaker_speech_path, do_save_embeddings=True):
+        self.encoder_manager.process_speaker(speaker_speech_path,
+                                             do_save_embeddings=do_save_embeddings)
+
+    def synthesize_spectrograms(self, texts, embeddings, do_save_spectrograms=True):
+        self.synthesizer_manager.synthesize_spectrograms(texts, embeddings,
+                                                         do_save_spectrograms=do_save_spectrograms)
+
+    def generate_waveform(self, mel, normalize=True, batched=True,
+                       target=8000, overlap=800, do_save_wav=True):
+        self.vocoder_manager.infer_waveform(mel, normalize=normalize,
+                                            batched=batched,
+                                            target=target,
+                                            overlap=overlap,
+                                            do_save_wav=do_save_wav)
---
+++ src/tts_modules/vocoder/vocoder_manager.py
@@ -0,0 +1,75 @@
+from vocoder.models.wavernn import WaveRNN
+from vocoder.configs import hparams as hp
+import torch
+import os
+
+
+class VocoderManager:
+
+    def __init__(self, configs, test_dataloader=None, train_dataloader=None,
+                 model=None, checkpoint_path=None):
+        self.configs = configs
+        self.test_dataloader = test_dataloader
+        self.train_dataloader = train_dataloader
+        self.model = model
+        self.checkpoint_path = checkpoint_path
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda")
+        else:
+            self.device = torch.device("cpu")
+
+        if self.model is None:
+            self.__init_wavernn()
+
+    def infer_waveform(self, mel, normalize=True, batched=True,
+                       target=8000, overlap=800, do_save_wav=True):
+        """
+        Infers the waveform of a mel spectrogram output by the synthesizer (the format must match
+        that of the synthesizer!)
+
+        :param normalize:
+        :param batched:
+        :param target:
+        :param overlap:
+        :return:
+        """
+        if self.model is None:
+            raise Exception("Load WaveRNN, please!")
+
+        if normalize:
+            mel = mel / hp.mel_max_abs_value
+        mel = torch.from_numpy(mel[None, ...])
+        wav = self.model.generate(mel, batched, target, overlap, hp.mu_law)
+        if do_save_wav:
+            save_wav(wav, os.path.join(self.configs.wav_save_path, 'result.wav'))
+        return wav
+
+
+    def __init_wavernn(self):
+        self.model = WaveRNN(
+            rnn_dims=hp.voc_rnn_dims,
+            fc_dims=hp.voc_fc_dims,
+            bits=hp.bits,
+            pad=hp.voc_pad,
+            upsample_factors=hp.voc_upsample_factors,
+            feat_dims=hp.num_mels,
+            compute_dims=hp.voc_compute_dims,
+            res_out_dims=hp.voc_res_out_dims,
+            res_blocks=hp.voc_res_blocks,
+            hop_length=hp.hop_length,
+            sample_rate=hp.sample_rate,
+            mode=hp.voc_mode
+        ).to(self.device)
+        if self.checkpoint_path is not None:
+            self.__load_model()
+        self.model.eval()
+
+    def __load_model(self):
+        if self.checkpoint_path is not None:
+            self.model.load(self.checkpoint_path)
+        else:
+            print('Vocoder was not loaded!!!')
+
+    def __save_checkpoint(self, path):
+        pass
+
